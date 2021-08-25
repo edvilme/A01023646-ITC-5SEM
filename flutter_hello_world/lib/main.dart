bca46e19b8b8cecd02bd9c8452df33a69530e3c4
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2021-08-24 22:44:58 -0500

    Added more interaction

--- flutter_hello_world/lib/main.dart
+++ flutter_hello_world/lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'dart:math';
+
+import 'package:flutter/services.dart';
 
 // Como en C, C++, Java, etc., el main es la primera función que se llama, y puede recibir argumentos desde la terminal
 void main(List<String> args) {
@@ -23,19 +27,22 @@ class HomePage extends StatefulWidget {
 	HomePageState createState() => HomePageState();
 }
 
+// Crea un color random
+Color createRandomColor(){
+	Random rmd = new Random();
+	return Color.fromRGBO(rmd.nextInt(255), rmd.nextInt(255), rmd.nextInt(255), 1);
+}
+String colorToHex(Color c){
+	String r = c.red.toRadixString(16).padLeft(2, '0').toUpperCase();
+	String g = c.green.toRadixString(16).padLeft(2, '0').toUpperCase();
+	String b = c.blue.toRadixString(16).padLeft(2, '0').toUpperCase();
+	return "#"+r+g+b;
+}
+
+
 // Acá se define el estado que irá cambiando
 class HomePageState extends State<HomePage>{
-	// Crea un color random
-	Color createRandomColor(){
-		Random rmd = new Random();
-		return Color.fromRGBO(rmd.nextInt(255), rmd.nextInt(255), rmd.nextInt(255), 1);
-	}
-	String colorToHex(Color c){
-		String r = c.red.toRadixString(16).padLeft(2, '0').toUpperCase();
-		String g = c.green.toRadixString(16).padLeft(2, '0').toUpperCase();
-		String b = c.blue.toRadixString(16).padLeft(2, '0').toUpperCase();
-		return "#"+r+g+b;
-	}
+
 	// Para poder hacer scroll
 	ScrollController _scrollController = new ScrollController();
 	void _scrollToTop(){
@@ -46,26 +53,63 @@ class HomePageState extends State<HomePage>{
 		return Scaffold(
 			appBar: AppBar(title: Text("Random color generator")),
 			body: ListView.builder(itemBuilder: (context, index) {
-				Color c = createRandomColor();
+				Color color = createRandomColor();
+				Color textColor = color.computeLuminance() > 0.5 ? Colors.black : Colors.white;
+				String hexColor = colorToHex(color);
 				return Container(
-					padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
 					decoration: BoxDecoration(
-						color: c
+						color: color
 					),
-					child: Text(
-						// "("+index.toString()+") "+c.toString(), 
-						'(${index.toString()}) ${colorToHex(c)}',
-						style: TextStyle(
-							color: c.computeLuminance() > 0.5 ? Colors.black : Colors.white
+					child: ListTile(
+						title: Text(hexColor,
+							style: TextStyle(color: textColor),
 						),
+						subtitle: Text(index.toString(),
+							style: TextStyle(color: textColor),
+						),
+						trailing: Icon(
+							Icons.chevron_right,
+							color: textColor,
+						),
+						onTap: (){
+							showModalBottomSheet<void>(context: context, builder: (BuildContext context) {
+								// return Text("Hi");
+								return Container(
+									decoration: BoxDecoration(
+										color: color
+									),
+									padding: const EdgeInsets.all(16),
+									child: Column(children: [
+										Text(hexColor, 
+											style: TextStyle(
+												fontSize: 24,
+												color: textColor
+											),
+										),
+										Text("Lorem ipsum dolor sit amet. Disfruté mucho hacer esta app en Flutter",
+											style: TextStyle(
+												color: textColor
+											),
+										),
+										ElevatedButton(
+											child: Text('Copy'),
+											onPressed: (){
+												Clipboard.setData(ClipboardData(text: hexColor));
+											},
+										)
+									])
+								);
+							});
+						}
 					),
 				);
 			}, controller: _scrollController),
 			floatingActionButton: FloatingActionButton(
 				onPressed: _scrollToTop,
-				tooltip: 'Increment',
+				tooltip: 'Go to top',
 				child: Icon(Icons.arrow_upward),
 			),
 		);
 	}
 }
+
